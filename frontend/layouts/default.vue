a74f4833c9d152c804a82b850ee01b6c373c8638
Leonardo Almeida <leoalmeidasa@gmail.com>
2022-03-30 09:40:27 -0300

    refactor(layout): add gradient background

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -12,7 +12,7 @@
 
 <style>
 body {
-    background-color:darkcyan;
-    color: black;
+  background: linear-gradient(to right, #292E49, #536976, #BBD2C5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  color: black;
   }
 </style>
