6c60398066046d2c8888c5933088c28a4a734190
Leonardo Almeida <leoalmeidasa@gmail.com>
2022-04-01 18:20:41 -0300

    feat(paginate): add paginate component

---
+++ frontend/components/pagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(page)">Previous</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(1)">1</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(2)">2</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(3)">3</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(4)">4</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="paginateFunc(page)">Next</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    paginateFunc: Function
+  },
+  data () {
+    return {
+      pagination: ''
+    }
+  }
+}
+</script>
+<style scoped>
+  nav {
+    margin-top: 0%;
+    margin-left: 18%;
+    cursor:pointer;
+  }
+</style>
