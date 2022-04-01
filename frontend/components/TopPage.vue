79d2059736049ba8836678deaabe727a7807c592
Leonardo Almeida <leoalmeidasa@gmail.com>
2022-04-01 18:04:00 -0300

    refactor(TopPage): Refactor navbar and style

--- frontend/components/TopPage.vue
+++ frontend/components/TopPage.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav id="top-navbar" class="navbar navbar-default navbar-fixed-top navbar-scroll navbar-scroll-fixed navbar-color" role="navigation">
-    <a class="navbar-brand">
-      <img class="logo" height="50" width="50" src="~/assets/download.png">
-    </a>
+  <nav id="top-navbar" class="navbar navbar-fixed-top navbar-scroll navbar-scroll-fixed navbar-color" role="navigation">
+    <div class="campanha-title-container">
+      <img class="logo" height="60" width="60" src="~/assets/img.jpeg">
+      <h5 class="campanha-title">
+        OM30
+      </h5>
+    </div>
     <span id="title-top" class="navbar-text"><strong>SICAM - SISTEMA CADASTRO DE MUNÍCIPES</strong></span>
   </nav>
 </template>
@@ -10,16 +13,32 @@
 <style scoped>
   #top-navbar {
     font-family: Roboto,sans-serif;
-    background: #B2EBF2;
-    margin-left: 18%;
+    background: #660924;
     flex-wrap: none;
-    padding-top: 0;
+    position:fixed;
+    width: 100%;
+    top: 0;
     padding-bottom: 0;
   }
   #title-top {
-    margin-left: 25%;
+    margin-right: 35%;
     text-align: center;
-    color:black;
+    color: white;
     margin-bottom: 1%;
+  }
+  .logo {
+    display: block;
+    max-height: 40px;
+  }
+  .campanha-title-container {
+    display: flex;
+    align-items: center;
+  }
+  .campanha-title-container .campanha-title {
+    color: #FFFFFF;
+    max-height: 17px;
+    -webkit-line-clamp: 1;
+    flex: 1;
+    padding-left: 13px;
   }
 </style>
